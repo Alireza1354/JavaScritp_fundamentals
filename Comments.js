f375a90e594fa0e712a11522c0ed80eb0572e107
Ali reza Karimi <karimiformlfco@hotmail.com>
2023-10-06 21:55:32 +0330

    Initial commit

---
+++ Comments.js
@@ -0,0 +1,20 @@
+
+// This is a single-line comment
+let firstName = "Alex";
+
+/*  This is a multi-line comment. You can 
+place more information in this syntax. */
+let lastName = "Karimi";
+
+
+/** 
+* Joins the first and last names together.
+*
+* @param {string} firstName The first name
+* @param {string} lastName The last name
+* @return {string} The full name
+*/
+function getFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`;
+}
+
---
+++ README.md
@@ -0,0 +1,11 @@
+## Why we use comments in JavaScript?
+
+- To help other humans understand the code.
+- To add metadata for tools that are analyzing our code.
+- Documentation and Configuration.
+
+## Keyboard shortcuts for comment our code
+
+- Single-line comment ---> ctrl + k + c
+- Multiple-lines comment ---> shift + alt + A
+- Format document in VS Code ---> shift + alt + f
