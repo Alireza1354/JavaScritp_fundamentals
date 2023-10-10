b88dad49c82720badf454461865c35413e790f4c
Alex <karimfromlfco@gmail.com>
2023-10-10 15:39:57 +0330

    Add variable file

---
+++ variables.js
@@ -0,0 +1,30 @@
+
+//  code conventions and the naming rules for variables
+
+// meaningful variable names
+let firstName = 'bob';
+let emailAddress = 'bob@gmail.com';
+
+// Camel casing
+let zipCode = 10;
+let ZipCode = 20;
+console.log(zipCode + ZipCode);
+
+
+/* We use const whenever we want to express our intent to 
+the javascript compiler that we do not intend for that variable 
+to ever change its value. */
+const j = 5;
+
+// the variable itself does not have a datatype
+let x;
+console.lot(x); //---> undefined
+console.log(typeof x); //---> undefined
+
+// A data type is really just the kind of data that we want to store
+
+
+
+
+
+
