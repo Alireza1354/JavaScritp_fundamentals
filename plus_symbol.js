3dfe4d0d2916c52ce0878df2dde6614e47f60f3b
Alex <karimfromlfco@gmail.com>
2023-10-10 15:39:31 +0330

    Add pluss symbol file

---
+++ plus_symbol.js
@@ -0,0 +1,10 @@
+
+/* The plus symbol has has double duty it's serving to be the addition operator 
+but it's also serving to concatenate two values together in this case
+to string values together so that we can print it out the screen. */
+
+let x = 8;
+let y = 2;
+let z = x + y;
+console.log('Answer: ' + z);
+
