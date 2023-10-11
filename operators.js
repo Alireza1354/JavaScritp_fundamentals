2b2da4d027d6f4f0aa215ea50f7c04098343c37a
Alex <karimfromlfco@gmail.com>
2023-10-11 15:34:42 +0330

    Add operator file

---
+++ operators.js
@@ -0,0 +1,29 @@
+// Caregories of operators
+
+// Assignment
+// Arithmetic + - * / %(modulus)---> remainder amount
+var m = 10 % 3;
+console.log(m);
+// Increment / Decrment ++ --
+var a = 1;
+a++;
+// console.log(a++);
+console.log(++a);
+console.log(a);
+
+// Precedence
+var b = 1 + 2 * 3
+var b = (1 + 2) * 3
+console.log(b);
+
+// Function invocation operators ()
+// console.log('')
+
+// Logical and: && or: ||
+
+// Member accessor operator.
+// console.log()
+
+// Code block operators { }
+
+// Array element access operators [] ---> Square brackets
