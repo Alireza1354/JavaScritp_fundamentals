d14de2a4d0983e9fa86432c774eaca681d735f1a
Alex <karimfromlfco@gmail.com>
2023-10-15 10:42:45 +0330

    Add some comments

--- variable-scope.js
+++ variable-scope.js
@@ -4,6 +4,9 @@
 
 // let = variables are limited to block scope {}
 // var = variables are limited to a function(){}
+
+// global variable = is declared outside any function
+// (if global, var will CHANGE browser's window properties.)
 //**************************************************
 
 // example:
