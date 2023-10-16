8542f1fdf2cd2eef2fbd8ce7b757afecdb76e45b
Alex <karimfromlfco@gmail.com>
2023-10-16 14:25:23 +0330

    Add some comments to number guessing

--- number-gussing-game.js
+++ number-gussing-game.js
@@ -1,9 +1,20 @@
 
 //----02:03:53---video
+
+// Answer will be a random number between 1 and 10
+
+/* const answer = Math.random() * 10; ---> this will give us a random number
+    between zero and nine.
+*/
+
+// const answer = Math.random() * 10 + 1; ---> Add 1 for numbers one through ten.
+
+
 const answer = Math.floor(Math.random() * 10 + 1);
 
 let guesses = 0;
 
+// Set the onClick event attribute equal to a function.
 document.getElementById("submitButton").onclick = function () {
 
     let guess = document.getElementById("guessField").value;
